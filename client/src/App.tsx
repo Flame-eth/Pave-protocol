a24086a4344a80c77b80113ca780c12814f168e1
Flame-eth <theflamepen@gmail.com>
2024-04-04 14:35:22 +0100

    Update CSS and add react-router-dom dependency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
 import './App.css'
+import {
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+  useLocation,
+} from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Layout = () => {
+    const path = useLocation().pathname;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [path]);
+    return (
+      <div className=" overflow-hidden">
+        {/* <Navbar /> */}
+        <Outlet />
+        {/* <Footer /> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+       
+      ],
+    },
+  
+   
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App
