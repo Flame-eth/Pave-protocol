317a5ca32fb2e111c04bb97d012f3a8ddf27a31a
Flame-eth <theflamepen@gmail.com>
2024-04-05 01:19:06 +0100

    Deploy Token and USDC contracts, UserAccountData and LendingPool deployment updates

--- web3/scripts/deploy.js
+++ web3/scripts/deploy.js
@@ -33,40 +33,27 @@ async function main() {
     console.log('Deploying contracts with the account:', deployer.address);
 
     // Deploy Token contract
-    // const Token = await hre.ethers.getContractFactory('PAPCoin');
-    // const token = await Token.deploy(1000000);
-    // await token.deployed();
+    const Token = await hre.ethers.getContractFactory('PAPCoin');
+    const token = await Token.deploy(1000000);
+    await token.deployed();
 
-    // // Deploy USDC contract
-    // const USDC = await hre.ethers.getContractFactory('PAPUSDC');
-    // const usdc = await USDC.deploy(1000000);
-    // await usdc.deployed();
+    // Deploy USDC contract
+    const USDC = await hre.ethers.getContractFactory('PAPUSDC');
+    const usdc = await USDC.deploy(1000000);
+    await usdc.deployed();
 
     // Deploy UserAccountData contract
-    // const UserAccountData = await hre.ethers.getContractFactory('UserAccountData');
-    // const userAccountData = await UserAccountData.deploy(token.address);
-    // await userAccountData.deployed();
-
-    // // Deploy LendingPool contract
-    // const LendingPool = await hre.ethers.getContractFactory('LendingPool');
-    // const lendingPool = await LendingPool.deploy(token.address, usdc.address, userAccountData.address);
-    // await lendingPool.deployed();
-
     const UserAccountData = await hre.ethers.getContractFactory('UserAccountData');
-    const userAccountData = await UserAccountData.deploy('0xC1CFB0eaF74729014E78c8cD90c52Be1922480D8');
+    const userAccountData = await UserAccountData.deploy(token.address);
     await userAccountData.deployed();
 
     // Deploy LendingPool contract
     const LendingPool = await hre.ethers.getContractFactory('LendingPool');
-    const lendingPool = await LendingPool.deploy(
-        '0xC1CFB0eaF74729014E78c8cD90c52Be1922480D8',
-        '0x0a0E99796f9671DB4Df676dE32189bda60951ab1',
-        userAccountData.address
-    );
+    const lendingPool = await LendingPool.deploy(token.address, usdc.address, userAccountData.address);
     await lendingPool.deployed();
 
-    // console.log('Token deployed to:', token.address);
-    // console.log('USDC deployed to:', usdc.address);
+    console.log('Token deployed to:', token.address);
+    console.log('USDC deployed to:', usdc.address);
     console.log('UserAccountData deployed to:', userAccountData.address);
     console.log('LendingPool deployed to:', lendingPool.address);
 }
